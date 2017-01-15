5a5ae83cddf116d23378a30bbffc8a722c3e5cdc
Philip Karasev <keroone17@gmail.com>
2017-01-15 17:00:14 +0200

    structure and core pcgs

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import SampleComponent from "./SampleComponent";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Movies</h2>
+                </div>
+                <SampleComponent />
+            </div>
+        );
+    }
+}
+
+export default App;
